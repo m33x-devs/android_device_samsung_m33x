5d3d8b5cb35b16805a9a90e560c1f8ebf9c4e246
Albert I <kras@raphielgang.org>
2023-12-26 01:32:35 +0100

    m33x: Setup dexpreopt configuration
    * Due to LineageOS/android_build@28d0698e8, eng and userdebug builds are
    only having boot and system server dexpreopt-ed thus need to explicitly
    disable WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY on device
    makefile to enable full dexpreopt for userdebug builds.
    
    * Also, remove useless WITH_DEXPREOPT flag from device makefile since it
    defaults to true in the build system and WILL never be set.
    
    Change-Id: I4de4585ee96bc9c289a15793b57f1911f0184d76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,14 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT ?= true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
+  endif
+endif
+
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2023-11-01
 
